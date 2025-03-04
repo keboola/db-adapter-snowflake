ecc3fb4b2949805e228b033e1ef36d12d52b682c
Marek Šuca <mark.suca@gmail.com>
2025-03-04 09:53:48 +0100

    cs

--- tests/phpunit/DSNBuilderTest.php
+++ tests/phpunit/DSNBuilderTest.php
@@ -70,12 +70,14 @@ FFdzKwQdMPWGCJTt0KdMw2s=
             'keyPair' => self::PRIVATE_KEY_PAIR,
         ]);
 
+        /** @codingStandardsIgnoreStart */
         self::assertTrue(
             @preg_match(
                 '/^Driver=SnowflakeDSIIDriver;Server=host;Port=443;Tracing=0;Database="database";Warehouse="warehouse";Role="role";AUTHENTICATOR=SNOWFLAKE_JWT;PRIV_KEY_FILE=\/tmp\/snowflake_private_key_([a-zA-Z0-9]*).p8;UID=user$/',
                 $dsn,
             ) === 1,
         );
+        /** @codingStandardsIgnoreEnd */
     }
 
     public function testInvalidKeyPair(): void
